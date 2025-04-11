0bd000c6eeaa1af0d319cae99cfaea1552418e20
Flagn3 <flagn3@gmail.com>
2025-04-11 09:11:27 +0200

    blocker issue S1219

---
+++ README.md
@@ -0,0 +1,7 @@
+# Examen Refactorización
+
+## Blocker issues (1)
+
+##### issue java:S1219
+Eliminamos del case 7 del switch el ```label{}```
+ 
--- src/examen/NotaFinalCalculator.java
+++ src/examen/NotaFinalCalculator.java
@@ -152,11 +152,8 @@ public class NotaFinalCalculator {
 
 		case 8:
 		case 7:
-			label: {
-				resultado = "Notable";
-				break;
-			}
-
+			resultado = "Notable";
+			break;
 		case 6:
 			resultado = "Bien";
 			break;
